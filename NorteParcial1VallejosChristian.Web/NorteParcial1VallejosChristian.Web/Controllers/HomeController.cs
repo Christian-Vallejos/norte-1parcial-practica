bfcf0a8f8cb432db986a7c2cba6b19d6c039d0fb
ChristianVallejos <vallejos.christiannahuel@gmail.com>
2020-05-30 16:03:05 -0300

    CSS diseno

--- NorteParcial1VallejosChristian.Web/NorteParcial1VallejosChristian.Web/Controllers/HomeController.cs
+++ NorteParcial1VallejosChristian.Web/NorteParcial1VallejosChristian.Web/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace NorteParcial1VallejosChristian.Web.Controllers
         {
             return View();
         }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
     }
 }
